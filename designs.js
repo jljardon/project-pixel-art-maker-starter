6af56b3450916dfa1a3b1f183ff84c205056ad8e
jljardon <github email address>
2018-04-10 15:07:43 -0400

    add sizePicker, makeGrid and addColor
    Added functionality to display a grid of the correct size and change cell color

--- designs.js
+++ designs.js
@@ -1,10 +1,33 @@
 // Select color input
 // Select size input
 
+var gridHeight, gridWidth, cellColor;
+var table = $('#pixelCanvas');
+
 // When size is submitted by the user, call makeGrid()
+$('#sizePicker').submit(function(event) {
+  event.preventDefault();
+  gridHeight = $('#inputHeight').val();
+  gridWidth = $('#inputWeight').val();
+  makeGrid(gridHeight, gridWidth);
+})
 
-function makeGrid() {
 
-// Your code goes here!
+
+function makeGrid(height, width) {
+
+  table.html("");
+
+  for (var r = 1; r <= height; r++) {
+    table.append('<tr id=row' + r + '></tr>');
+    for (var d = 1; d <= width; d++) {
+      $('#row' + r).append('<td></td>');
+    }
+  }
+
+  $('td').click(function addColor() {
+    cellColor = $('#colorPicker').val();
+    $(this).attr('style', 'background-color:' + cellColor);
+  })
 
 }
